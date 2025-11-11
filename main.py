740914e02159c54555e1290176531846efc7f20b
Carlos Valero <93935336+ZenTanE@users.noreply.github.com>
2025-11-11 11:25:50 +0100

    add: main

---
+++ main.py
@@ -0,0 +1,11 @@
+from developer import Developer
+
+dev = Developer("Carlos Valero", 
+                ["English", "Spanish", "Catalan"], 
+                "carlosvalerosala2020@gmail.com", 
+                "649 86 38 05", 
+                "https://www.linkedin.com/in/carlos-valero-sala/", 
+                "https://github.com/ZenTanE/CV/", 
+                ["Python", "Java", "TypeScript", "HTML5", "SQL", "C#"], 
+                "I\'m an empathetic, pragmatic and dedicated developer")
+print(dev)
